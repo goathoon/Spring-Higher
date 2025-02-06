27e8db75c8ac3ef6a77d9e5966f8f26f99d96fb6
Goathoon <ownest2@gmail.com>
2025-02-06 23:35:21 +0900

    동적프록시jdk 사용하여 Controller,Service,Repository에 로그 기능 적용 및 필터링하기

--- src/main/java/hello/proxy/ProxyApplication.java
+++ src/main/java/hello/proxy/ProxyApplication.java
@@ -4,6 +4,8 @@ import hello.proxy.config.AppV1Config;
 import hello.proxy.config.AppV2Config;
 import hello.proxy.config.v1_proxy.ConcreteProxyConfig;
 import hello.proxy.config.v1_proxy.InterfaceProxyConfig;
+import hello.proxy.config.v2_dynamicproxy.DynamicProxyBasicConfig;
+import hello.proxy.config.v2_dynamicproxy.DynamicProxyFilterConfig;
 import hello.proxy.trace.logtrace.LogTrace;
 import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
@@ -12,7 +14,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 
 //@Import({AppV1Config.class,AppV2Config.class})
-@Import(ConcreteProxyConfig.class)
+//@Import(ConcreteProxyConfig.class)
+@Import(DynamicProxyFilterConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.proxy.app.v3") //v3부터는 컴포넌트 스캔대상으로 등록 (@RestController가 컴포넌트 스캔 대상이 되므로 v1,v2는 제외시키기 위함)
 // 추가로 Config 패키지를 import하지 않으려고 app쪽 패키지만 scan대상으로 지정
 public class ProxyApplication {
@@ -26,3 +29,4 @@ public class ProxyApplication {
 		return new ThreadLocalLogTrace();
 	}
 }
+
